016f42635129f84e5344fc80b0fe2a7465292873
Delon00 <djplebob32@gmail.com>
2023-08-30 02:45:17 +0100

    new

--- public/js/js.js
+++ public/js/js.js
@@ -1,22 +1,13 @@
-//------------------------MODAL
+//------------------------MENU
 const toggleLabel = document.querySelector('.toggle');
-  // Sélectionnez le modal
-  const modal = document.getElementById('myModal');
-
-  // Ajoutez un gestionnaire d'événements click à l'étiquette
-  toggleLabel.addEventListener('click', function() {
-    // Ajoutez ou supprimez la classe "show" pour contrôler la visibilité du modal
-    modal.classList.toggle('show');
+const menu = document.getElementById('myModal');
+toggleLabel.addEventListener('click', function(){
+    menu.classList.add('show');
   });
-
-  // // Sélectionnez l'élément de fermeture du modal
-  // const closeModal = document.querySelector('.close');
-
-  // // Ajoutez un gestionnaire d'événements click à l'élément de fermeture du modal
-  // closeModal.addEventListener('click', function() {
-  //   // Supprimez la classe "show" pour masquer le modal
-  //   modal.classList.remove('show');
-  // });
+const closeMenu = document.querySelector('.close');
+closeMenu.addEventListener('click', function() {
+  menu.classList.remove('show');
+});
 
 //------------------------CONEXION SLIDE
 const signUpButton = document.getElementById('signUp');
